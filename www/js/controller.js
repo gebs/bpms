6e2a0b85ef2d910bcfc611ff264afaa0cdd0cf33
psychonoob <adrian.gebs@stud.hslu.ch>
2017-03-31 20:27:44 +0200

    Erweiterung Model

--- www/js/controller.js
+++ www/js/controller.js
@@ -7,17 +7,25 @@ function init(){
 
 function fillPatientInfo(patient){
     clearPatientInfo();
-    $("#paddress").val(patient.address);
-    $("#pzipcity").val(patient.zip + ' ' +patient.city);
-    $("#chief_complaint").val(patient.chief_complaint);
-    $("#history_of_currentIllness").val(patient.history_of_present_illness);
+    $("#paddress").html(patient.address);
+    $("#pzipcity").html(patient.zip + ' ' +patient.city);
+    $("#chief_complaint").html(patient.chief_complaint);
+    $("#booldType").html("<b>BloodType:</b>" + patient.blood_type);
+     $.each(patient.allergies,function(i,a){
+        $("#allergies").append("<li>" + a+ "</li>");
+    });
+    
+    $("#history_of_currentIllness").html(patient.history_of_present_illness);
     $.each(patient.mediacal_history,function(i,a){
         $("#mediacal_history").append("<li>" + a+ "</li>");
     });
     $.each(patient.surgical_history,function(i,a){
         $("#surgical_history").append("<li>" + a+ "</li>");
     });
-    $("#social_history").val(patient.social_history);
+    $("#social_history").html(patient.social_history);
+    $.each(patient.appointments,function(i,a){
+        $("#appointments").append("<li>" + a.date + "&#9; " +a.appointment +"</li>");
+    });
 }
 
 function clearPatientInfo(){
@@ -27,5 +35,8 @@ function clearPatientInfo(){
     $("#history_of_currentIllness").empty();
     $("#mediacal_history").empty();
     $("#surgical_history").empty();
-    $("#social_history").empty();
+    $("#booldType").empty();
+    $("#allergies").empty();
+    $("#appointments").empty();
+    
 }
--- www/js/model.js
+++ www/js/model.js
@@ -28,6 +28,15 @@ var patients = [
         address: "Musterstrasse 1",
         zip: 6000,
         city: "Luzern",
+        blood_type: "AB-",
+        allergies: [
+            "latex"
+        ],
+        medicine:[
+            "Aspirin",
+            "Parazetamol",
+            "opium"
+        ],
         chief_complaint: "swelling of tongue and difficulty breathing and swallowing",
         history_of_present_illness: "77 y o woman in NAD with a h/o CAD, DM2, asthma and HTN on altace for 8 years awoke from sleep around 2:30 am this morning of a sore throat and swelling of tongue.She came immediately to the ED b/c she was having difficulty swallowing and some trouble breathing due to obstruction caused by the swelling. She has never had a similar reaction ever before and she did not have any associated SOB, chest pain, itching, or nausea. She has not noticed any rashes, and has been afebrile. She says that she feels like it is swollen down in her esophagus as well. In the ED she was given 25mg benadryl IV, 125 mg solumedrol IV and pepcid 20 mg IV. This has helped the swelling some but her throat still hurts and it hurts to swallow. Nothing else was able to relieve the pain and nothing make it worse though she has not tried to drink any fluids because of trouble swallowing. She denies any recent travel, recent exposure to unusual plants or animals or other allergens. She has not started any new medications, has not used any new lotions or perfumes and has not eaten any unusual foods. Patient has not taken any of her oral medications today.",
         mediacal_history: [
@@ -45,7 +54,19 @@ var patients = [
             "s/p hystarectomy in 1970s ",
             "s/p kidney stone retrieval 1960s "
         ],
-        social_history: "Patient lives in _______ with daughter _____ (919) _______. Patient does all ADLs and IADLs with no/little assistance. She does own finances and drives. Patient has 4 daughters that all live in the area. Patient does not use tobacco, alcohol, illicit drugs. "
+        social_history: "Patient lives in _______ with daughter _____ (919) _______. Patient does all ADLs and IADLs with no/little assistance. She does own finances and drives. Patient has 4 daughters that all live in the area. Patient does not use tobacco, alcohol, illicit drugs. ",
+        appointments:[
+            {
+                date:"14.12.2016",
+                appointment: "X-Ray"
+            },{
+                date:"15.12.2016",
+                appointment:"physiotherapy"
+            },{
+                date:"20.12.2016",
+                appointment:"surgery"
+            }
+        ]
 
     }
 ]
